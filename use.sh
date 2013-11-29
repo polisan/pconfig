6019e937176dcbafeb4ecb0548cc047d86dd0ebd
Hongchang Li <lihch@yahoo-inc.com>
2013-11-29 09:39:43 +0000

    enable use.sh

--- use.sh
+++ use.sh
@@ -1,20 +1,64 @@
 #!/bin/bash
 
-function use_vim() {
+PCONFIG_ROOT=`pwd`
+os_distr="rh"
+
+function detect_os()
+{
+    os_distr="rh"
 }
 
-
-function use_tmux() {
+function symlink_file()
+{
+    orig=$1
+    if [ ! -f "$orig" ]; then
+        return
+    fi
+    target=$2
+    if [ -f "$target" ]; then
+        rm $target
+    fi
+    ln -sf $orig $target
 }
 
-function use_bash() {
+function symlink_dir()
+{
+    orig=$1
+    if [ ! -d "$orig" ]; then
+        return
+    fi
+    target=$2
+    if [ -d "$target" ]; then
+        rm -rf $target
+    fi
+    ln -sf $orig $target
 }
 
-function use_screen() {
+function use_vim() 
+{
+    symlink_file $PCONFIG_ROOT/modules/vim/vimrc.${os_distr} ~/.vimrc
+    symlink_dir $PCONFIG_ROOT/modules/vim/vim_files ~/.vim
+}
+
+function use_tmux() 
+{
+    symlink_file $PCONFIG_ROOT/modules/tmux/tmux.conf.${os_distr} ~/.tmux.conf
+}
+
+function use_bash() 
+{
+    symlink_file $PCONFIG_ROOT/modules/bash/bashrc.${os_distr} ~/.bashrc
+}
+
+function use_screen() 
+{
+    symlink_file $PCONFIG_ROOT/modules/screen/screenrc.${os_distr} ~/.screenrc
+
 }
 
 
 command=$1
+detect_os
 
 
 case $command in
