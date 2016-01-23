fb979472b57976bbec116b6439608c46ee5dae2a
hongchang li <leehongchang@gmail.com>
2016-01-23 20:49:40 +0800

    add hosts.d

---
+++ mac_config/_gnu.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+brew install binutils
+brew install diffutils
+brew install ed --default-names
+brew install findutils --default-names
+brew install gawk
+brew install gnu-indent --default-names
+brew install gnu-sed --default-names
+brew install gnu-tar --default-names
+brew install gnu-which --default-names
+brew install gnutls --default-names
+brew install grep --default-names
+brew install gzip
+brew install watch
+brew install wdiff --with-gettext
+brew install wget
+
+brew install bash
+brew install gpatch
+brew install m4
+brew install nano
+
+
+brew install file-formula
+brew install git
+brew install less
+brew install openssh --with-brewed-openssl
+brew install rsync
+brew install svn
+brew install unzip
--- modules/hosts.d/combine.sh
+++ modules/hosts.d/combine.sh
@@ -2,6 +2,7 @@
 CWD=`dirname $0`
 CONFIG_FILE="$CWD/sequence.conf"
 TEMP_FILE="/tmp/hosts.`date +'%Y%m%d%H%M'`"
+
 while read line; do
     cat $CWD/hosts.$line >> $TEMP_FILE
 done < $CONFIG_FILE
