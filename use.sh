c1c420231491b862cc394d807c1e18977adb467b
lihongchang <lihongchang@babeltime.com>
2016-01-23 23:42:29 +0800

    add tpm for tmux

--- use.sh
+++ use.sh
@@ -9,63 +9,39 @@ function detect_os()
     echo "not implemented!"
 }
 
-function symlink_file()
+function symlink()
 {
-    orig=$1
-    if [ ! -f "$orig" ]; then
-        return
-    fi
-    target=$2
-    if [ -f "$target" ]; then
-        rm $target
-    fi
-    ln -sf $orig $target
-}
-
-function symlink_dir()
-{
-    orig=$1
-    if [ ! -d "$orig" ]; then
-        return
-    fi
-    target=$2
-    if [ -d "$target" ]; then
-        rm -rf $target
-    fi
-    ln -sf $orig $target
+    ln -snf $1 $2 
 }
 
 function use_vim() 
 {
-    symlink_file $PCONFIG_ROOT/modules/vim/vimrc ~/.vimrc
-    symlink_dir $PCONFIG_ROOT/modules/vim/vim_files ~/.vim
+    symlink $PCONFIG_ROOT/modules/vim/vimrc ~/.vimrc
+    symlink $PCONFIG_ROOT/modules/vim/vim_files ~/.vim
 }
 
 function use_tmux() 
 {
-    if [ "$os_distr" == "mac" ]; then
-        symlink_dir $PCONFIG_ROOT/local.mac $PCONFIG_ROOT/local
-    else
-        symlink_dir $PCONFIG_ROOT/local.rh $PCONFIG_ROOT/local
-    fi
-    symlink_file $PCONFIG_ROOT/modules/tmux/tmux.conf.${os_distr} ~/.tmux.conf
+    symlink $PCONFIG_ROOT/local.${os_distr} $PCONFIG_ROOT/local
+    symlink $PCONFIG_ROOT/modules/tmux/tmux.conf.${os_distr} ~/.tmux.conf
+    symlink $PCONFIG_ROOT/modules/tmux/tmux ~/.tmux
 }
 
 function use_bash() 
 {
     if [ "$os_distr" == "mac" ]; then
-        symlink_file $PCONFIG_ROOT/modules/bash/bashrc.${os_distr} ~/.bashrc
-        symlink_file $PCONFIG_ROOT/modules/bash/bash_profile.${os_distr} ~/.bash_profile
+        symlink $PCONFIG_ROOT/modules/bash/bashrc.${os_distr} ~/.bashrc
+        symlink $PCONFIG_ROOT/modules/bash/bash_profile.${os_distr} ~/.bash_profile
     else 
-        symlink_file $PCONFIG_ROOT/modules/bash/bashrc.${os_distr} ~/.bashrc
-        symlink_file $PCONFIG_ROOT/modules/bash/bash_profile.${os_distr} ~/.bash_profile
+        symlink $PCONFIG_ROOT/modules/bash/bashrc.${os_distr} ~/.bashrc
+        symlink $PCONFIG_ROOT/modules/bash/bash_profile.${os_distr} ~/.bash_profile
     fi
     echo "use_bash done"
 }
 
 function use_screen() 
 {
-    symlink_file $PCONFIG_ROOT/modules/screen/screenrc.${os_distr} ~/.screenrc
+    symlink $PCONFIG_ROOT/modules/screen/screenrc.${os_distr} ~/.screenrc
 
 }
 
